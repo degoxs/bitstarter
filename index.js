29d80580b4745d7436086756909a5c76cce7e14d
degoxs <issac_degollado@hotmail.com>
2014-10-05 15:50:15 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-var express = require('express')
-var app = express();
+var express = require('express');
+var app = express.createServer(express.logger());
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.get('/',function(request,response){
+    response.send('Hello World 2!');
+});
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var port = process.env.PORT || 5000;
+app.listen(port,function(){
+    console.log("listening on " + port);
+});
