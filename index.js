9fe11ab126aa8ccd8f7417df93ec917630a2cb49
degoxs <issac_degollado@hotmail.com>
2014-10-05 23:13:09 -0500

    programa que le un html

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var outfile = "index.html";
-var out;
-fs.writeFileSync(outfile,out);
+var outfile = 'index.html';
+var out=fs.readFileSync(outfile);
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
@@ -11,5 +10,5 @@ app.get('/', function(request, response) {
   response.send(out);
 });
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:"+ out  + app.get('port'))
+  console.log("Node app is running at localhost:"+outfile+ "  " + out + app.get('port'));
 });
