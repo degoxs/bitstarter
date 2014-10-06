c85d8104e8afcfb7d8c7342d6ff8016b54667be7
degoxs <issac_degollado@hotmail.com>
2014-10-05 22:47:09 -0500

    Lee un html

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var fs = require('fs');
+var outfile = "index.html";
+var out;
+fs.wirteFileSync(outfile,out);
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
-})
-
+  response.send(out);
+});
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:"+ out  + app.get('port'))
+});
