d0887021ffccddb7e7c2e474143e5f2e9a53a692
degoxs <issac_degollado@hotmail.com>
2014-10-05 23:21:59 -0500

    Con comentarios para prueba

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
-var outfile = 'index.html';
-var out=fs.readFileSync(outfile);
+//var fs = require('fs');
+//var outfile = 'index.html';
+//var out=fs.readFileSync(outfile);
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(out);
+  response.send("Hello  world");
 });
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:"+outfile+ "  " + out + app.get('port'));
+  console.log("Node app is running at localhost:" app.get('port'));
 });
