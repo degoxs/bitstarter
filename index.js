176c2075460ed2c23695d910f572a15380d675e2
degoxs <issac_degollado@hotmail.com>
2014-10-05 23:34:18 -0500

    no especificado el tipo o regresa un buffer que no es un string

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var outfile = 'index.html';
-//var out=fs.readFileSync(outfile);
+var out = fs.readFileSync(outfile,'utf-8');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
